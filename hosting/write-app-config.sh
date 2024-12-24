f3d013a451cab884dc67ef30e0fe407f46ee1449
David Haley <dchaley@gmail.com>
2024-12-23 21:47:20 -0800

    Generate .firebaserc from environment
    Also, switch Angular's app config to use a template.

--- hosting/write-app-config.sh
+++ hosting/write-app-config.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-jq 'with_entries((.value | select(startswith("$"))) |= env[.[1:]])' \
-  < src/app/reservations-app.config.json.template \
+jq -n \
+  --arg FIREBASE_PROJECT_ID "$FIREBASE_PROJECT_ID" \
+  --arg FIREBASE_APP_ID "$FIREBASE_APP_ID" \
+  --arg FIREBASE_STORAGE_BUCKET "$FIREBASE_STORAGE_BUCKET" \
+  --arg FIREBASE_API_KEY "$FIREBASE_API_KEY" \
+  --arg FIREBASE_AUTH_DOMAIN "$FIREBASE_AUTH_DOMAIN" \
+  --arg FIREBASE_MESSAGING_SENDER_ID "$FIREBASE_MESSAGING_SENDER_ID" \
+  -f src/app/reservations-app.config.json.jq \
   > src/app/reservations-app.config.json
