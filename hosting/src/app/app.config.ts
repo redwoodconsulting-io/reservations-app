f15e27103afef6ebeb5ebc3f46d986f133cc190c
David Haley <dchaley@gmail.com>
2024-12-25 22:11:24 -0800

    Connect to emulated auth + firestore

--- hosting/src/app/app.config.ts
+++ hosting/src/app/app.config.ts
@@ -3,10 +3,11 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
-import {getAuth, provideAuth} from '@angular/fire/auth';
-import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {connectAuthEmulator, getAuth, provideAuth} from '@angular/fire/auth';
+import {connectFirestoreEmulator, getFirestore, provideFirestore} from '@angular/fire/firestore';
 
 import reservationsAppConfig from './reservations-app.config.json';
+import {environment} from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,7 +21,19 @@ export const appConfig: ApplicationConfig = {
       "authDomain": reservationsAppConfig.authDomain,
       "messagingSenderId": reservationsAppConfig.messagingSenderId,
     })),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
+    provideAuth(() => {
+      const auth = getAuth();
+      if (environment.useEmulators) {
+        connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
+      }
+      return auth;
+    }),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      if (environment.useEmulators) {
+        connectFirestoreEmulator(firestore, 'localhost', 8080)
+      }
+      return firestore;
+    }),
   ],
 };
