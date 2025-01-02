25f8d4f28582811811300c3b7b14db3dee7a0bbc
David Haley <dchaley@gmail.com>
2025-01-01 21:04:56 -0800

    Capture reservation audit logs
    Each time a reservation is created, updated, or deleted, record a
    document in the reservationsAuditLog collection.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,67 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+import {onDocumentWrittenWithAuthContext} from "firebase-functions/v2/firestore";
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const {initializeApp} = require('firebase-admin/app');
+const {getFirestore} = require('firebase-admin/firestore');
+
+initializeApp();
+const db = getFirestore();
+
+export const modifyDocument =
+  onDocumentWrittenWithAuthContext("reservations/{reservationId}",
+    (event) => {
+      const docId = event.data?.before.id || event.data?.after.id;
+      logger.info(`Reservation changed: ${docId}`);
+
+      const changes: { [key: string]: any } = {};
+
+      // Track the id becoming absent or present to
+      // mean the document being deleted or created
+      if (event.data?.before.data() && !event.data?.after.data()) {
+        changes.id = {before: docId};
+      }
+      if (event.data?.after.data() && !event.data?.before.data()) {
+        changes.id = {after: docId};
+      }
+
+      const previousValues = event.data?.before.data() || {};
+      const newValues = event.data?.after.data() || {};
+
+      Object.entries(previousValues).forEach(([key, value]) => {
+        const newValue = newValues[key];
+        if (key !== 'id' && newValue !== value) {
+          changes[key] = {
+            before: value,
+          };
+          if (newValue !== undefined) {
+            changes[key].after = newValue;
+          }
+        }
+      });
+      Object.entries(newValues).forEach(([key, value]) => {
+        if (key !== 'id' && previousValues[key] === undefined) {
+          changes[key] = {
+            after: value,
+          };
+        }
+      });
+
+      const {authType, authId} = event;
+      const who = authType === "system" ? "System" : (authId ? authId : "Unknown");
+
+      const startDate = ((newValues.startDate || previousValues.startDate) as string);
+
+      db.collection("reservationsAuditLog").doc().create({
+        reservationId: docId,
+        changes,
+        who: who,
+        year: Number((startDate.substring(0, 4))),
+        timestamp: new Date(),
+      }).then(() => {
+        logger.info("Reservation audit log created");
+      }).catch((error: any) => {
+        logger.error(`Error creating audit log: ${error}`);
+      });
+    }
+  );
